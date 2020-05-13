fdd0efe46df588f9283f67412056ed20c93aec9e
oliverfeher <oliver92.feher@gmail.com>
2020-05-12 22:17:56 -0400

    Test DB with InsertMany

--- mongodb.js
+++ mongodb.js
@@ -18,9 +18,56 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => 
 
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne({
-        name: "Oliver",
-        age: 27
+    // db.collection("users").insertOne({
+    //     name: "Oliver",
+    //     age: 27
+    // }, (error, result) => {
+    //     if (error)
+    //     {
+    //         return console.log("Unable to insert user!");
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection("user").insertMany([
+    //     {
+    //         name: "Jen",
+    //         age: 28
+    //     },
+    //     {
+    //         name: "Gunther",
+    //         age: 40
+    //     }
+    // ], (error, result) => {
+    //     if (error)
+    //     {
+    //         return console.log("Unable to insert documents");
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection("tasks").insertMany([
+        {
+            description: "cook dinner",
+            completed: true
+        },
+        {
+            description: "feed the dog",
+            completed: false
+        },
+        {
+            description: "work out",
+            completed: false
+        }
+    ], (error, result) => {
+        if (error)
+        {
+            return console.log("Unable to instert tasks!");
+        }
+
+        console.log(result.ops);
     })
 })
 
